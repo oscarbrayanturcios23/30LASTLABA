c42e480729d9bfc57b882b9e4ab61672cfe453de
Brayan Fuentes <oscarbrayanturcios23@gmail.com>
2017-08-14 18:18:39 -0600

    Add files via upload

---
+++ Nombres/src/nombres/Nombres.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nombres;
+import java.util.Scanner;
+/**
+ *
+ * @author estudiante
+ */
+public class Nombres {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String Nom="";
+        
+        System.out.println("Ingrese Nombre");
+        Scanner a = new Scanner (System.in);
+        Nom= a.nextLine();
+  
+        System.out.println(Nom);
+    }
+    
+}
